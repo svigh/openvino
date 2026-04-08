svigh/openvino

commit 64a330242e291e372e37f4344316deb63f6474fe

[Protopipe] Introduce ConfigNode wrapper for YAML config validation (#35124)
### Details:
- *Introduced `ConfigNode` wrapper around `YAML::Node` to track which
config keys are accessed during parsing*
- *Unused keys are reported as warnings at the end of config parsing,
helping users catch typos*
 - *Added `LOG_WARN` log level to the logger for appropriate severity*

### Tickets:
 - *EISW-159287*

### AI Assistance:
 - *AI assistance used: yes*
- *GitHub Copilot was used briefly for code review and suggesting C++
idioms. Implementation, design decisions, and all testing were done
manually.*

src/plugins/intel_npu/tools/protopipe/src/parser/config.cpp
@@ -140,7 +140,7 @@ namespace YAML {
 
 template <typename T>
 struct convert<std::vector<T>> {
-    static bool decode(const Node& node, std::vector<T>& vec) {
+    static bool decode(const ConfigNode& node, std::vector<T>& vec) {
         if (!node.IsSequence()) {
             return false;
         }
@@ -154,7 +154,7 @@ struct convert<std::vector<T>> {
 
 template <typename K, typename V>
 struct convert<std::map<K, V>> {
-    static bool decode(const Node& node, std::map<K, V>& map) {
+    static bool decode(const ConfigNode& node, std::map<K, V>& map) {
         if (!node.IsMap()) {
             return false;
         }
@@ -167,7 +167,7 @@ struct convert<std::map<K, V>> {
 
 template <typename T>
 struct convert<LayerVariantAttr<T>> {
-    static bool decode(const Node& node, LayerVariantAttr<T>& layer_attr) {
+    static bool decode(const ConfigNode& node, LayerVariantAttr<T>& layer_attr) {
         // Note: "metric" and "random" entries in config are always presented
         //       as maps.
         //       To differ passed variants for them between "one value for all layers"
@@ -192,7 +192,7 @@ struct convert<LayerVariantAttr<T>> {
 
 template <>
 struct convert<UniformGenerator::Ptr> {
-    static bool decode(const Node& node, UniformGenerator::Ptr& generator) {
+    static bool decode(const ConfigNode& node, UniformGenerator::Ptr& generator) {
         if (!node["low"]) {
             THROW_ERROR("Uniform distribution must have \"low\" attribute");
         }
@@ -207,7 +207,7 @@ struct convert<UniformGenerator::Ptr> {
 
 template <>
 struct convert<IRandomGenerator::Ptr> {
-    static bool decode(const Node& node, IRandomGenerator::Ptr& generator) {
+    static bool decode(const ConfigNode& node, IRandomGenerator::Ptr& generator) {
         if (!node["dist"]) {
             THROW_ERROR("\"random\" must have \"dist\" attribute!");
         }
@@ -223,7 +223,7 @@ struct convert<IRandomGenerator::Ptr> {
 
 template <>
 struct convert<Norm::Ptr> {
-    static bool decode(const Node& node, Norm::Ptr& metric) {
+    static bool decode(const ConfigNode& node, Norm::Ptr& metric) {
         // NB: If bigger than tolerance - fail.
         if (!node["tolerance"]) {
             THROW_ERROR("Metric \"norm\" must have \"tolerance\" attribute!");
@@ -236,7 +236,7 @@ struct convert<Norm::Ptr> {
 
 template <>
 struct convert<Cosine::Ptr> {
-    static bool decode(const Node& node, Cosine::Ptr& metric) {
+    static bool decode(const ConfigNode& node, Cosine::Ptr& metric) {
         // NB: If lower than threshold - fail.
         if (!node["threshold"]) {
             THROW_ERROR("Metric \"cosine\" must have \"threshold\" attribute!");
@@ -249,7 +249,7 @@ struct convert<Cosine::Ptr> {
 
 template <>
 struct convert<NRMSE::Ptr> {
-    static bool decode(const Node& node, NRMSE::Ptr& metric) {
+    static bool decode(const ConfigNode& node, NRMSE::Ptr& metric) {
         // NB: If bigger than tolerance - fail.
         if (!node["tolerance"]) {
             THROW_ERROR("Metric \"nrmse\" must have \"tolerance\" attribute!");
@@ -262,7 +262,7 @@ struct convert<NRMSE::Ptr> {
 
 template <>
 struct convert<IAccuracyMetric::Ptr> {
-    static bool decode(const Node& node, IAccuracyMetric::Ptr& metric) {
+    static bool decode(const ConfigNode& node, IAccuracyMetric::Ptr& metric) {
         const auto type = node["name"].as<std::string>();
         if (type == "norm") {
             metric = node.as<Norm::Ptr>();
@@ -279,7 +279,7 @@ struct convert<IAccuracyMetric::Ptr> {
 
 template <>
 struct convert<GlobalOptions> {
-    static bool decode(const Node& node, GlobalOptions& opts) {
+    static bool decode(const ConfigNode& node, GlobalOptions& opts) {
         if (node["model_dir"]) {
             if (!node["model_dir"]["local"]) {
                 THROW_ERROR("\"model_dir\" must contain \"local\" key!");
@@ -317,7 +317,7 @@ struct convert<GlobalOptions> {
 
 template <>
 struct convert<OpenVINOParams> {
-    static bool decode(const Node& node, OpenVINOParams& params) {
+    static bool decode(const ConfigNode& node, OpenVINOParams& params) {
         // FIXME: Worth to separate these two
         const auto name = node["name"] ? node["name"].as<std::string>() : node["path"].as<std::string>();
         fs::path path{name};
@@ -387,7 +387,7 @@ struct convert<OpenVINOParams> {
 
 template <>
 struct convert<ONNXRTParams::OpenVINO> {
-    static bool decode(const Node& node, ONNXRTParams::OpenVINO& ov_ep) {
+    static bool decode(const ConfigNode& node, ONNXRTParams::OpenVINO& ov_ep) {
         if (node["params"]) {
             ov_ep.params_map = node["params"].as<std::map<std::string, std::string>>();
         }
@@ -406,7 +406,7 @@ struct convert<ONNXRTParams::OpenVINO> {
 
 template <>
 struct convert<ONNXRTParams::EP> {
-    static bool decode(const Node& node, ONNXRTParams::EP& ep) {
+    static bool decode(const ConfigNode& node, ONNXRTParams::EP& ep) {
         const auto ep_name = node["name"].as<std::string>();
         if (ep_name == "OV") {
             ep = node.as<ONNXRTParams::OpenVINO>();
@@ -419,7 +419,7 @@ struct convert<ONNXRTParams::EP> {
 
 template <>
 struct convert<ONNXRTParams> {
-    static bool decode(const Node& node, ONNXRTParams& params) {
+    static bool decode(const ConfigNode& node, ONNXRTParams& params) {
         // FIXME: Worth to separate these two
         params.model_path = node["name"] ? node["name"].as<std::string>() : node["path"].as<std::string>();
         if (node["session_options"]) {
@@ -440,7 +440,7 @@ struct convert<ONNXRTParams> {
 
 template <>
 struct convert<Network> {
-    static bool decode(const Node& node, Network& network) {
+    static bool decode(const ConfigNode& node, Network& network) {
         // NB: Take path stem as network tag
         // Note that at this point, it's fine if names aren't unique
         const auto name = node["name"].as<std::string>();
@@ -475,7 +475,7 @@ struct convert<Network> {
 
 template <>
 struct convert<CPUOp> {
-    static bool decode(const Node& node, CPUOp& op) {
+    static bool decode(const ConfigNode& node, CPUOp& op) {
         // TODO: Assert there are no more options provided
         op.time_in_us = node["time_in_us"] ? node["time_in_us"].as<uint64_t>() : 0u;
         return true;
@@ -484,7 +484,7 @@ struct convert<CPUOp> {
 
 template <>
 struct convert<InferOp> {
-    static bool decode(const Node& node, InferOp& op) {
+    static bool decode(const ConfigNode& node, InferOp& op) {
         const auto framework = node["framework"] ? node["framework"].as<std::string>() : "openvino";
         if (framework == "openvino") {
             // NB: Parse OpenVINO model parameters such as path, device, precision, etc
@@ -514,7 +514,7 @@ struct convert<InferOp> {
 
 template <>
 struct convert<OpDesc> {
-    static bool decode(const Node& node, OpDesc& opdesc) {
+    static bool decode(const ConfigNode& node, OpDesc& opdesc) {
         opdesc.tag = node["tag"].as<std::string>();
         auto type = node["type"] ? node["type"].as<std::string>() : "Infer";
         auto repeat_count = node["repeat_count"] ? node["repeat_count"].as<uint64_t>() : 1u;
@@ -549,7 +549,7 @@ struct convert<OpDesc> {
 
 }  // namespace YAML
 
-static std::vector<std::vector<Network>> parseNetworks(const YAML::Node& node) {
+static std::vector<std::vector<Network>> parseNetworks(const ConfigNode& node) {
     ASSERT(node.IsSequence());
     TagsManager tgs_mngr;
     std::vector<std::vector<Network>> networks_list;
@@ -654,7 +654,7 @@ static InferenceParams adjustParams(InferenceParams&& params, const GlobalOption
     return adjustParams(std::get<ONNXRTParams>(std::move(params)), opts);
 }
 
-static void parseWorkloadType(const YAML::Node& node, StreamDesc& stream) {
+static void parseWorkloadType(const ConfigNode& node, StreamDesc& stream) {
     WorkloadTypeDesc workload_type;
     if (node["initial_value"]) {
         workload_type.initial_value = node["initial_value"].as<std::string>();
@@ -683,7 +683,7 @@ static void parseWorkloadType(const YAML::Node& node, StreamDesc& stream) {
     stream.workload_type = std::make_optional(workload_type);
 }
 
-static StreamDesc parseStream(const YAML::Node& node, const GlobalOptions& opts, const std::string& default_name,
+static StreamDesc parseStream(const ConfigNode& node, const GlobalOptions& opts, const std::string& default_name,
                               const ReplaceBy& replace_by) {
     StreamDesc stream;
 
@@ -813,7 +813,7 @@ static ScenarioGraph buildGraph(const std::vector<OpDesc>& op_descs,
     return graph;
 }
 
-static StreamDesc parseAdvancedStream(const YAML::Node& node, const GlobalOptions& opts,
+static StreamDesc parseAdvancedStream(const ConfigNode& node, const GlobalOptions& opts,
                                       const std::string& default_name, const ReplaceBy& replace_by) {
     StreamDesc stream;
 
@@ -877,7 +877,7 @@ static StreamDesc parseAdvancedStream(const YAML::Node& node, const GlobalOption
     return stream;
 }
 
-static std::vector<StreamDesc> parseStreams(const YAML::Node& node, const GlobalOptions& opts,
+static std::vector<StreamDesc> parseStreams(const ConfigNode& node, const GlobalOptions& opts,
                                             const ReplaceBy& replace_by) {
     std::vector<StreamDesc> streams;
     uint32_t stream_idx = 0;
@@ -891,7 +891,7 @@ static std::vector<StreamDesc> parseStreams(const YAML::Node& node, const Global
     return streams;
 }
 
-static std::vector<ScenarioDesc> parseScenarios(const YAML::Node& node, const GlobalOptions& opts,
+static std::vector<ScenarioDesc> parseScenarios(const ConfigNode& node, const GlobalOptions& opts,
                                                 const ReplaceBy& replace_by) {
     std::vector<ScenarioDesc> scenarios;
     for (const auto& subnode : node) {
@@ -913,7 +913,7 @@ static std::vector<ScenarioDesc> parseScenarios(const YAML::Node& node, const Gl
     return scenarios;
 }
 
-Config parseConfig(const YAML::Node& node, const ReplaceBy& replace_by) {
+Config parseConfig(const ConfigNode& node, const ReplaceBy& replace_by) {
     const auto global_opts = node.as<GlobalOptions>();
 
     // FIXME: Perhaps should be done somewhere else...

src/plugins/intel_npu/tools/protopipe/src/parser/config.hpp
@@ -1,4 +1,3 @@
-//
 // Copyright (C) 2018-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -6,7 +5,6 @@
 #pragma once
 
 #include "parser/parser.hpp"
+#include "parser/config_node.hpp"
 
-#include <yaml-cpp/yaml.h>
-
-Config parseConfig(const YAML::Node& root, const ReplaceBy& replace_by);
+Config parseConfig(const ConfigNode& root, const ReplaceBy& replace_by);

src/plugins/intel_npu/tools/protopipe/src/parser/config_node.hpp
@@ -1,0 +1,200 @@
+// Copyright (C) 2018-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <yaml-cpp/yaml.h>
+
+#include "utils/logger.hpp"
+
+#include <memory>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <type_traits>
+#include <utility>
+
+class ConfigNode {
+public:
+    ConfigNode(YAML::Node&& node, bool isRoot = false): _node(std::move(node)), _isRoot(isRoot) {
+        collectKeys();
+    }
+
+    ~ConfigNode() {
+        if (_isRoot) {
+            std::vector<std::string> unusedKeys = getUnusedKeys();
+            if (!unusedKeys.empty()) {
+                std::string unusedKeysStr = "Unused config keys: ";
+                for (size_t i = 0; i < unusedKeys.size(); ++i) {
+                    unusedKeysStr += unusedKeys[i];
+                    if (i < unusedKeys.size() - 1) unusedKeysStr += " ";
+                }
+                LOG_WARN() << unusedKeysStr << std::endl;
+            }
+        }
+    }
+
+    ConfigNode(const ConfigNode&) = delete;
+    ConfigNode& operator=(const ConfigNode&) = delete;
+
+    ConfigNode(ConfigNode&& other) = delete;
+    ConfigNode& operator=(ConfigNode&& other)  = delete;
+
+    bool IsSequence() const { return _node.IsSequence(); }
+    bool IsMap() const { return _node.IsMap(); }
+    std::size_t size() const { return _node.size(); }
+
+    template <typename T>
+    T as() const {
+        // For primitive types, use YAML's converter
+        if constexpr (std::is_arithmetic_v<T> || std::is_same_v<T, std::string>) {
+            return _node.as<T>();
+        } else {
+            T result;
+            if (!YAML::convert<T>::decode(*this, result)) {
+                THROW_ERROR("Failed to convert config node to requested type");
+            }
+            return result;
+        }
+    }
+
+    template <typename Key>
+    ConfigNode& operator[](const Key& key);
+
+    template <typename Key>
+    const ConfigNode& operator[](const Key& key) const;
+
+    struct ConfigKey {
+        YAML::Node _node;
+
+        template <typename T>
+        T as() const { return _node.as<T>(); }
+    };
+
+    struct KeyValueProxy {
+        ConfigKey first;
+        const ConfigNode& second;
+
+        const KeyValueProxy* operator->() const { return this; }
+
+        bool IsSequence() const { return second.IsSequence(); }
+        bool IsMap() const { return second.IsMap(); }
+        std::size_t size() const { return second.size(); }
+
+        template <typename T>
+        T as() const { return second.as<T>(); }
+
+        template <typename Key>
+        const ConfigNode& operator[](const Key& key) const { return second[key]; }
+
+        explicit operator bool() const { return static_cast<bool>(second); }
+
+        operator const ConfigNode&() const { return second; }
+    };
+
+    class const_iterator {
+    public:
+        const_iterator(ConfigNode& parent, std::size_t index)
+            : _parent(parent), _index(index) {
+            if (_parent._node.IsMap()) {
+                cacheKeysAndNodes();
+            }
+        }
+
+        KeyValueProxy operator*() const {
+            if (_parent._node.IsMap()) {
+                const auto& [keyStr, keyNode] = _cachedKeys[_index];
+                return KeyValueProxy{ConfigKey{keyNode}, _parent[keyStr]};
+            } else {
+                return KeyValueProxy{ConfigKey{YAML::Node()}, _parent[_index]};
+            }
+        }
+
+        KeyValueProxy operator->() const {
+            return operator*();
+        }
+
+        const_iterator& operator++() {
+            ++_index;
+            return *this;
+        }
+
+        bool operator!=(const const_iterator& other) const {
+            return _index != other._index;
+        }
+
+    private:
+        void cacheKeysAndNodes() {
+            if (_cachedKeys.empty()) {
+                for (const auto& kv : _parent._node) {
+                    _cachedKeys.emplace_back(kv.first.as<std::string>(), kv.first);
+                }
+            }
+        }
+
+        ConfigNode& _parent;
+        std::size_t _index;
+        std::vector<std::pair<std::string, YAML::Node>> _cachedKeys;
+    };
+
+    const_iterator begin() const {
+        return const_iterator(const_cast<ConfigNode&>(*this), 0);
+    }
+
+    const_iterator end() const {
+        return const_iterator(const_cast<ConfigNode&>(*this), _node.size());
+    }
+
+    explicit operator bool() const { return _node.IsDefined() && !_node.IsNull(); }
+
+private:
+    void collectKeys() {
+        if (_node.IsMap()) {
+            for (const auto& kv : _node) {
+                _keys.insert(kv.first.as<std::string>());
+            }
+        }
+    }
+
+    std::vector<std::string> getUnusedKeys() const {
+        std::vector<std::string> unused(_keys.begin(), _keys.end());
+        for (const auto& [key, child] : _children) {
+            auto childUnused = child->getUnusedKeys();
+            unused.insert(unused.end(), childUnused.begin(), childUnused.end());
+        }
+        return unused;
+    }
+
+    YAML::Node _node;
+    bool _isRoot;
+    mutable std::unordered_set<std::string> _keys;
+    mutable std::unordered_map<std::string, std::unique_ptr<ConfigNode>> _children;
+};
+
+template <typename Key>
+const ConfigNode& ConfigNode::operator[](const Key& key) const {
+    std::string keyStr;
+    if constexpr (std::is_convertible_v<Key, std::string>) {
+        keyStr = std::string(key);
+    } else {
+        keyStr = std::to_string(key);
+    }
+
+    _keys.erase(keyStr);
+
+    auto it = _children.find(keyStr);
+    if (it == _children.end()) {
+        auto child = std::make_unique<ConfigNode>(YAML::Clone(_node[key]));
+        auto [inserted_it, success] = _children.emplace(keyStr, std::move(child));
+        return *inserted_it->second;
+    }
+
+    return *it->second;
+}
+
+template <typename Key>
+ConfigNode& ConfigNode::operator[](const Key& key) {
+    return const_cast<ConfigNode&>(std::as_const(*this)[key]);
+}

src/plugins/intel_npu/tools/protopipe/src/parser/parser.cpp
@@ -1,10 +1,10 @@
-//
 // Copyright (C) 2018-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
 #include "parser/parser.hpp"
 #include "parser/config.hpp"
+#include "parser/config_node.hpp"
 
 #include <yaml-cpp/yaml.h>
 
@@ -12,7 +12,7 @@ ScenarioParser::ScenarioParser(const std::string& filepath): m_filepath(filepath
 }
 
 Config ScenarioParser::parseScenarios(const ReplaceBy& replace_by) {
-    const auto root = YAML::LoadFile(m_filepath);
+    const ConfigNode root = {YAML::LoadFile(m_filepath), true};
     // TODO: Extend to any other config syntax
     return parseConfig(root, replace_by);
 }

src/plugins/intel_npu/tools/protopipe/src/utils/logger.cpp
@@ -1,4 +1,3 @@
-//
 // Copyright (C) 2018-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -19,6 +18,9 @@ std::stringstream& Logger::stream() {
 Logger::~Logger() {
     if (m_lvl <= Logger::global_lvl) {
         switch (m_lvl) {
+        case LogLevel::Warn:
+            std::cout << "[ WARN ] " << m_ss.str();
+            break;
         case LogLevel::Info:
             std::cout << "[ INFO ] " << m_ss.str();
             break;

src/plugins/intel_npu/tools/protopipe/src/utils/logger.hpp
@@ -1,4 +1,3 @@
-//
 // Copyright (C) 2018-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -9,8 +8,9 @@
 
 enum class LogLevel {
     None = 0,
-    Info = 1,
-    Debug = 2,
+    Warn = 1,
+    Info = 2,
+    Debug = 3,
 };
 
 class Logger {
@@ -25,5 +25,6 @@ private:
     std::stringstream m_ss;
 };
 
+#define LOG_WARN() Logger{LogLevel::Warn}.stream()
 #define LOG_INFO() Logger{LogLevel::Info}.stream()
 #define LOG_DEBUG() Logger{LogLevel::Debug}.stream()

src/plugins/intel_npu/tools/protopipe/version.hpp.in
@@ -1,3 +1,3 @@
 #pragma once
 
-#define APP_VERSION   "v1.2.3-@GIT_SHA@"
+#define APP_VERSION   "v1.2.4-@GIT_SHA@"

commit 5584cc3a39c58da3bc4a5a9b93c27510eaca6bba

[Protopipe] Fix const reference binding in config parser
This fixes the following compile error, introduced by commit
64a330242e291e372e37f4344316deb63f6474fe:

```
src/plugins/intel_npu/tools/protopipe/src/parser/config.cpp:148:9: error: cannot bind non-const lvalue reference of type ‘ConfigNode::KeyValueProxy&’ to an rvalue of type ‘ConfigNode::KeyValueProxy’
  148 |         for (auto& child : node) {
      |         ^~~
```

Signed-off-by: Daniel Bermond <dbermond@archlinux.org>

src/plugins/intel_npu/tools/protopipe/src/parser/config.cpp
@@ -145,7 +145,7 @@ struct convert<std::vector<T>> {
             return false;
         }
 
-        for (auto& child : node) {
+        for (const auto& child : node) {
             vec.push_back(child.as<T>());
         }
         return true;